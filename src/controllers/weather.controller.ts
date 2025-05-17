f6fe0b8f1e04823a1ceb2ba8ace1c9599fe4439b
Volodymyr Beimuk <tetrenkuchioan@gmail.com>
2025-05-17 09:14:01 -0400

    simplify weather controller

--- src/controllers/weather.controller.ts
+++ src/controllers/weather.controller.ts
@@ -2,6 +2,7 @@
 import { Request, Response } from 'express';
 import { getWeatherForCity } from '@services/weather.service';
 
+
 export const getWeather = async (req: Request, res: Response) => {
   const city = req.query.city as string;
 
@@ -9,14 +10,6 @@ export const getWeather = async (req: Request, res: Response) => {
     return res.status(400).json({ error: 'City is required' });
   }
 
-  try {
-    // TODO: Integrate real weather API (e.g., WeatherAPI.com)
-    // TODO: add 404 for city not found
-    const weather = await getWeatherForCity(city);
-
-    return res.status(200).json(weather);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Failed to fetch weather' });
-  }
+  const weather = await getWeatherForCity(city);
+  return res.status(200).json(weather);
 };
