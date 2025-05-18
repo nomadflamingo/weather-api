5bc08662d4738547ac4560bfcd63ab77e6a7150e
Volodymyr Beimuk <tetrenkuchioan@gmail.com>
2025-05-18 15:02:18 -0400

    added swagger docs

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import subscriptionRoutes from '@routes/subscription.routes';
 import { errorHandler } from '@middlewares/error.middleware';
 import { startWeatherEmailJob } from '@jobs/weather-email.job';
 import { rateLimiter } from '@middlewares/rate-limiter.middleware';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 dotenv.config();
 
@@ -14,10 +16,23 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Swagger UI
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, { explorer: true })
+);
+
+// Redirect root to API docs
+app.get('/', (_req, res) => {
+  res.redirect('/api-docs');
+});
+
 // Rate Limiter
 app.use(rateLimiter);
 
-// Routes
+// Main Routes
 app.use('/api/weather', weatherRoutes);
 app.use('/api', subscriptionRoutes); // subscribe, confirm, unsubscribe
 
